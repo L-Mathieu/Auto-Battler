e6aed3f5b6ae8fb92a5b28d294de16e0314d5f90
PapyChartreuse <m.louveaux@gmail.com>
2026-07-08 10:13:22 +0200

    Heal priority
    Amélioration de la gestion des priorité de la comptétence Heal

--- Skills/Heal.cs
+++ Skills/Heal.cs
@@ -23,10 +23,29 @@ namespace Auto_Battler.Skills
         public override int GetPriority(Character caster)
         {
             var priority = 0;
-            var usableSkills = caster.Team.Members
-                .Where(s => s.HP < s.MaxHP/2)
-                .ToList();
-            priority = usableSkills.Count > 0 ? 30 : -1;
+            var injuredCharacter = caster.Team.Members
+                .MinBy(s => s.HP/s.MaxHP);
+            switch ((injuredCharacter.HP / injuredCharacter.MaxHP) * 100)
+            {
+                case >= 100:
+                    priority = -1;
+                    break;
+                case > 75:
+                    priority = 1;
+                    break;
+                case > 50:
+                    priority = 15;
+                    break;
+                case > 25:
+                    priority = 50;
+                    break;
+                case > 10:
+                    priority = 100;
+                    break;
+                case > 1:
+                    priority = 1000;
+                    break;
+            }
             return priority;
         }
     }
