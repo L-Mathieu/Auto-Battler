aa9b5b556981f8732dbae32c060130e03bd61b5d
PapyChartreuse <m.louveaux@gmail.com>
2026-09-09 10:52:26 +0200

    EquipmentSave et EquipmentMapper

---
+++ Application/Mappers/EquipmentMapper.cs
@@ -0,0 +1,38 @@
+﻿using Auto_Battler.Application.Models;
+using Auto_Battler.Domain.Equipment;
+
+namespace Auto_Battler.Application.Mappers
+{
+    public class EquipmentMapper
+    {
+        public EquipmentItem ToEquipmentItem(EquipmentSave equipmentSave)
+        {
+            return new EquipmentItem
+            (
+                equipmentSave.Name,
+                equipmentSave.Level,
+                equipmentSave.RequiredLevel,
+                equipmentSave.EquipType,
+                equipmentSave.RequiredClass,
+                equipmentSave.AttackBonus,
+                equipmentSave.DefenceBonus,
+                equipmentSave.SpeedBonus
+            );
+        }
+
+        public EquipmentSave ToEquipmentSave(EquipmentItem equipmentItem)
+        {
+            return new EquipmentSave
+            {
+                Name = equipmentItem.Name,
+                Level = equipmentItem.Level,
+                RequiredLevel = equipmentItem.RequiredLevel,
+                EquipType = equipmentItem.EquipType,
+                RequiredClass = equipmentItem.RequiredClass,
+                AttackBonus = equipmentItem.AttackBonus,
+                DefenceBonus = equipmentItem.DefenceBonus,
+                SpeedBonus = equipmentItem.SpeedBonus
+            };
+        }
+    }
+}
---
+++ Application/Models/EquipmentSave.cs
@@ -0,0 +1,27 @@
+﻿using Auto_Battler.Domain.Equipment;
+using Auto_Battler.Domain.Hero;
+
+namespace Auto_Battler.Application.Models
+{
+    public class EquipmentSave
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Level { get; set; }
+
+        public int RequiredLevel { get; set; }
+
+        public EquipType EquipType { get; set; }
+
+        public HeroClass RequiredClass { get; set; }
+
+        public double AttackBonus { get; set; }
+
+        public double DefenceBonus { get; set; }
+
+        public double SpeedBonus { get; set; }
+
+    }
+}
